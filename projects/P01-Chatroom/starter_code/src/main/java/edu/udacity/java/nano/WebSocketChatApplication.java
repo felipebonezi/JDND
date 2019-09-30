b5769b31576a73e23e6ace106894ff3e28702e6f
Felipe Bonezi <1410319+felipebonezi@users.noreply.github.com>
2019-09-30 18:43:36 -0300

    :construction: Improve yaml configuration.

--- projects/P01-Chatroom/starter_code/src/main/java/edu/udacity/java/nano/WebSocketChatApplication.java
+++ projects/P01-Chatroom/starter_code/src/main/java/edu/udacity/java/nano/WebSocketChatApplication.java
@@ -1,6 +1,7 @@
 package edu.udacity.java.nano;
 
 import edu.udacity.java.nano.model.User;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,6 +14,9 @@ import java.net.UnknownHostException;
 @SpringBootApplication
 @RestController
 public class WebSocketChatApplication {
+
+    @Value("${app.webSocketUrl}")
+    private String webSocketUrl;
 
     public static void main(String[] args) {
         SpringApplication.run(WebSocketChatApplication.class, args);
@@ -32,6 +36,7 @@ public class WebSocketChatApplication {
     @GetMapping("/index")
     public ModelAndView index(String username, HttpServletRequest request) throws UnknownHostException {
         ModelAndView modelAndView = new ModelAndView("chat");
+        modelAndView.addObject("webSocketUrl", this.webSocketUrl);
         modelAndView.addObject("user", new User(username));
         return modelAndView;
     }
